444db17871c9fdeeed1c5224f8019518960429bd
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 11:33:54 -0300

    feat: creat front-end configs to show cards with back-end data

--- frontend/cardapio/src/App.tsx
+++ frontend/cardapio/src/App.tsx
@@ -1,12 +1,22 @@
 
 import './App.css'
+import { Card } from './components/card/card';
+import type { FoodData } from './interface/FoodData';
 
 function App() {
+  const data: FoodData[] = [];
+
   return (
     <div className="container">
       <h1>Cardápio</h1>
       <div className="card-grid">
-        
+        {data.map(foodData => 
+        <Card 
+          price={foodData.price} 
+          title={foodData.title} 
+          image={foodData.image}
+          />
+        )}
       </div>
     </div>
   )
