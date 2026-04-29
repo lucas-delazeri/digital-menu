7bd65b8673d7a9d3f006dc3b6269fef9b3799693
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 15:36:33 -0300

    feat: add new item modal functionality

--- frontend/cardapio/src/App.tsx
+++ frontend/cardapio/src/App.tsx
@@ -1,36 +1,46 @@
-
-import './App.css'
+import './App.css';
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
-import {useState} from "react";
-import {CreateModal} from "./components/create-modal/create-modal.tsx";
+import { useState } from 'react';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
-  const { data } = useFoodData();
-  const [isModalOpen, setIsModalOpen] = useState(false)
+    const { data } = useFoodData();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleOpenModal = () => {
-      setIsModalOpen(prev => !prev)
-  }
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
 
-  return (
-    <div className="container">
-      <h1>Cardápio</h1>
-      <div className="card-grid">
-        {data?.map(foodData =>
-            <Card
-                key={foodData.id}
-                id={foodData.id}
-                price={foodData.price}
-                title={foodData.title}
-                image={foodData.image}
-            />
-        )}
-      </div>
-        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
-        <button onClick={handleOpenModal}>novo</button>
-    </div>
-  )
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div className="container">
+            <button className="new-item" onClick={handleOpenModal}>
+                Novo item
+            </button>
+
+            <h1>Cardápio</h1>
+
+            <div className="card-grid">
+                {data?.map((foodData) => (
+                    <Card
+                        key={foodData.id}
+                        id={foodData.id}
+                        price={foodData.price}
+                        title={foodData.title}
+                        image={foodData.image}
+                    />
+                ))}
+            </div>
+
+            {isModalOpen && (
+                <CreateModal closeModal={handleCloseModal} />
+            )}
+        </div>
+    );
 }
 
-export default App
+export default App;
