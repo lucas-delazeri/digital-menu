ee2405ead3ffea6f76df29c9950f5fa6d5d2958b
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 14:33:33 -0300

    feat: close modal after checking it was submitted

--- frontend/cardapio/src/components/create-modal/create-modal.tsx
+++ frontend/cardapio/src/components/create-modal/create-modal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { useFoodDataMutate } from "../../hooks/useFoodDataMutate";
 import type { FoodData } from "../../interface/FoodData";
 
@@ -8,6 +8,10 @@ interface InputProps {
     label: string;
     value: string | number;
     updateValue: (value: any) => void;
+}
+
+interface ModalProps {
+    closeModal(): void;
 }
 
 const Input = ({ label, value, updateValue }: InputProps) => {
@@ -22,11 +26,11 @@ const Input = ({ label, value, updateValue }: InputProps) => {
     );
 };
 
-export function CreateModal() {
+export function CreateModal({ closeModal }: ModalProps) {
     const [title, setTitle] = useState("");
     const [price, setPrice] = useState(0);
     const [image, setImage] = useState("");
-    const { mutate } = useFoodDataMutate();
+    const { mutate, isSuccess } = useFoodDataMutate();
 
     const submit = () => {
         const foodData: FoodData = {
@@ -38,6 +42,10 @@ export function CreateModal() {
         mutate(foodData);
     };
 
+    useEffect(() => {
+        if (!isSuccess) return
+            closeModal()
+    }, [isSuccess])
 
     return (
         <div className="modal-overlay">
