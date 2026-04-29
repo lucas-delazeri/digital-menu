0069d057eb201bc96a39d0138f77a7f77d606a41
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 11:55:06 -0300

    feat: loading data from backend to frontend

--- frontend/cardapio/src/App.tsx
+++ frontend/cardapio/src/App.tsx
@@ -1,22 +1,23 @@
 
 import './App.css'
 import { Card } from './components/card/card';
-import type { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
 
   return (
     <div className="container">
       <h1>Cardápio</h1>
       <div className="card-grid">
-        {data.map(foodData => 
-        <Card 
-          price={foodData.price} 
-          title={foodData.title} 
-          image={foodData.image}
+        {data?.map((foodData) => (
+          <Card
+            key={foodData.id}
+            price={foodData.price}
+            title={foodData.title}
+            image={foodData.image}
           />
-        )}
+        ))}
       </div>
     </div>
   )
