b4d907e23123144144f5218503e1a101581cdd38
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 14:05:06 -0300

    feat: create modal to add a new item using useFoodDataMutate hook

---
+++ frontend/cardapio/src/components/create-modal/create-modal.tsx
@@ -0,0 +1,46 @@
+import {useState} from "react";
+import {useFoodDataMutate} from "../../hooks/useFoodDataMutate.ts";
+import type {FoodData} from "../../interface/FoodData.tsx";
+
+interface InputProps {
+    label: string,
+    value: string | number,
+    updateValue: (value: any) => void
+}
+
+const Input = ({ label, value, updateValue } : InputProps) => {
+    return (
+        <>
+            <label>{label}</label>
+            <input value={value} onChange={e => updateValue(e.target.value)}></input>
+        </>
+    )
+}
+
+export function CreateModal() {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [image, setImage] = useState("");
+    const { mutate } = useFoodDataMutate();
+
+    const submit = () => {
+        const foodData: FoodData = {
+            title,
+            price,
+            image
+        }
+        mutate(foodData)
+    }
+
+    return (
+        <div className="modal-overflow">
+            <h2>Cadastre um novo item no cardápio</h2>
+            <form className="input-container">
+                <Input label="title" value={title} updateValue={setTitle}/>
+                <Input label="price" value={price} updateValue={setPrice}/>
+                <Input label="image" value={image} updateValue={setImage}/>
+            </form>
+            <button onClick={submit} className="btn-secondary">Postar</button>
+        </div>
+    )
+}
