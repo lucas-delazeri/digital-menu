a1c93114e92c581569d63f3efb8a20f0111ec5d9
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 11:58:37 -0300

    feat: configure CORS for frontend integration

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -6,16 +6,20 @@ import com.example.cardapio.food.FoodRequestDTO;
 import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("food")
 public class FoodController {
     // (Autowired) Quando instanciar FoodController injeta a dependência
     @Autowired
     private FoodRepository repository;
-
+    // (CrossOrigin) Determina quais origens e quais headers serão passados na requisição
+    // Melhorar segurança partindo disso...
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public void saveFood(@RequestBody FoodRequestDTO data) {
         Food foodData = new Food(data);
