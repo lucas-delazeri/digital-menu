addd5bda721f7891c415dc0f537afdd26f795299
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 14:08:29 -0300

    feat: add 'CreateModal' component on App.tsx file

--- frontend/cardapio/src/App.tsx
+++ frontend/cardapio/src/App.tsx
@@ -2,23 +2,32 @@
 import './App.css'
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import {useState} from "react";
+import {CreateModal} from "./components/create-modal/create-modal.tsx";
 
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+      setIsModalOpen(prev => !prev)
+  }
 
   return (
     <div className="container">
       <h1>Cardápio</h1>
       <div className="card-grid">
-        {data?.map((foodData) => (
+        {data?.map(foodData =>
           <Card
             key={foodData.id}
             price={foodData.price}
             title={foodData.title}
             image={foodData.image}
           />
-        ))}
+        )}
       </div>
+        {isModalOpen && <CreateModal/>}
+        <button onClick={handleOpenModal}>novo</button>
     </div>
   )
 }
