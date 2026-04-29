e9d785f0c95dfa6cd0626a07c08847769c02d2bd
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 10:32:27 -0300

    feat: add FoodResponseDTO on food list

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -2,6 +2,7 @@ package com.example.cardapio.controller;
 
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,13 @@ import java.util.List;
 @RestController
 @RequestMapping("food")
 public class FoodController {
+    // (Autowired) Quando instanciar FoodController injeta a dependência
     @Autowired
     private FoodRepository repository;
     @GetMapping
-    public List<Food> getAll() {
+    public List<FoodResponseDTO> getAll() {
 
-        List<Food> foodList = repository.findAll();
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
     }
 }
