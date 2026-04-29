1444b353db284d69ea53dc298b30ac9c2b27b4fb
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 14:55:10 -0300

    feat: add delete icon on food cards

--- frontend/cardapio/src/components/card/card.tsx
+++ frontend/cardapio/src/components/card/card.tsx
@@ -9,9 +9,10 @@ interface CardProps {
 export function Card({ price, image, title }: CardProps) {
     return (
         <div className="card">
-            <img src={image}/>
+            <img className="delete-icon" src="/icon-close.png" alt="fechar" />
+            <img className="food-image" src={image} alt={title} />
             <h2>{title}</h2>
-            <p><b>Valor:</b>{price}</p>
+            <p><b>Valor:</b> {price}</p>
         </div>
-    )
+    );
 }
