5500fc5d4d3b357a44cc08393f6af724dcde7c77
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-29 11:22:34 -0300

    feat: create card component

--- frontend/cardapio/src/components/card/card.tsx
+++ frontend/cardapio/src/components/card/card.tsx
@@ -1,9 +1,17 @@
-interface CardProps {
+import './card.css';
 
+interface CardProps {
+    price: number;
+    title: string;
+    image: string;
 }
 
-export function Card(props: CardProps) {
+export function Card({ price, image, title }: CardProps) {
     return (
-        <></>
+        <div className="card">
+            <img/>
+            <h2></h2>
+            <p><b>Valor:</b></p>
+        </div>
     )
 }
